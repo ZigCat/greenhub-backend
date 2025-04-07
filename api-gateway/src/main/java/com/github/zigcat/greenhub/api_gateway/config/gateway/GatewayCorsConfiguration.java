71c66b4c368899df3a703bc58ab65b28f59a7653
ZigCat <zigcat517@gmail.com>
2025-04-07 13:53:54 +0500

    refactor: api-gateway cors config

---
+++ api-gateway/src/main/java/com/github/zigcat/greenhub/api_gateway/config/gateway/GatewayCorsConfiguration.java
@@ -0,0 +1,27 @@
+package com.github.zigcat.greenhub.api_gateway.config.gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+@Configuration
+public class GatewayCorsConfiguration {
+    @Value("${greenhub.gateway.origin}")
+    private String FRONT_ORIGIN;
+
+    @Bean
+    public CorsWebFilter corsWebFilter() {
+        CorsConfiguration corsConfig = new CorsConfiguration();
+        corsConfig.addAllowedOrigin(FRONT_ORIGIN);
+        corsConfig.addAllowedMethod("*");
+        corsConfig.addAllowedHeader("*");
+        corsConfig.setAllowCredentials(true);
+        corsConfig.setMaxAge(3600L);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfig);
+        return new CorsWebFilter(source);
+    }
+}
